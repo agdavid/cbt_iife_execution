7c76f89e8dd471b303f7ef0d0de06dce0e0cea5d
agdavid <antonio.g.david@gmail.com>
2016-09-21 08:32:17 -0400

    add fourth iife execution - alert with embedded iife

--- js/iife.js
+++ js/iife.js
@@ -8,11 +8,19 @@
 //   }
 // )(40, 50)
 
-var d = (function(a, b){
+// var d = (function(a, b){
+//   var c = 0;
+//   c = a - b;
+//   return c
+//   }
+// )(40, 5);
+
+// alert("The difference is:" + d);
+
+
+alert("The difference between the two is:" + (function(a, b){
   var c = 0;
   c = a - b;
   return c
   }
-)(40, 5);
-
-alert("The difference is:" + d);
+)(40, 5));
