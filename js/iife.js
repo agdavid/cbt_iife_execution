0ebc99f4e67c6210a8e66637952245d8740318ef
agdavid <antonio.g.david@gmail.com>
2016-09-21 08:26:33 -0400

    add second iife execution - with variables

--- js/iife.js
+++ js/iife.js
@@ -1,4 +1,9 @@
-(function() {
-  alert("This alert was immediately invoked!");
+// (function() {
+//   alert("This alert was immediately invoked!");
+//   }
+// )()
+
+(function(a, b){
+  alert("The sum of" + a.toString() + " and " + b.toString() + " is: " + (a+b).toString());
   }
-)()
+)(40, 50)
